a128b57fc0cbe5f4b0adb2299409ca9993e70bcd
Thomas Neos <tneos@hotmail.com>
2025-06-19 21:22:55 +0100

    Lists project, declaration, initialization and methods practice, initial commit

---
+++ ListApp/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ListApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare a list and initialize
+            List<string> colors = new List<string>();
+            // Add items
+            colors.Add("red");
+            colors.Add("blue");
+            colors.Add("yellow");
+            colors.Add("red");
+            colors.Add("red");
+            colors.Add("red");
+
+
+            // Remove item
+            bool isDeletingSuccessful = colors.Remove("red");
+
+            // As long as successful deletion repeat action
+            while (isDeletingSuccessful)
+            {
+                isDeletingSuccessful = colors.Remove("red");
+            }
+
+             Console.WriteLine("Current colours in the list");
+            foreach (string color in colors)
+            {
+                Console.WriteLine(color);
+            }
+        }
+    }
+}
