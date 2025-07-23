9dc3a73a8ee226351afb1de70bad5a56560e2eb5
Thomas Neos <tneos@hotmail.com>
2025-07-23 17:30:16 +0100

    Latest apps commit

--- Dictionary/obj/Debug/net9.0/Dictionary.AssemblyInfo.cs
+++ Dictionary/obj/Debug/net9.0/Dictionary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dictionary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d97f39c6f036cbf758d128577034d19ce8ea260")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60be5dab41a88633267241b2f3b9d2818d60cb70")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dictionary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dictionary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ListApp/obj/Debug/net9.0/ListApp.AssemblyInfo.cs
+++ ListApp/obj/Debug/net9.0/ListApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ListApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d97f39c6f036cbf758d128577034d19ce8ea260")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60be5dab41a88633267241b2f3b9d2818d60cb70")]
 [assembly: System.Reflection.AssemblyProductAttribute("ListApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ListApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
