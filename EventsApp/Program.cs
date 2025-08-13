d9f3c8818b2fd9f1f8588c7dd395c51a50df29bb
Thomas Neos <tneos@hotmail.com>
2025-08-13 11:11:09 +0100

    Add EventArgs in previous example

--- EventsApp/Program.cs
+++ EventsApp/Program.cs
@@ -1,11 +1,25 @@
 ﻿namespace EventsApp
 {
     // Delegate declaration(Signature of method that handles the event)
-    public delegate void TemperatureChangeHandler(string message);
+    //public delegate void TemperatureChangeHandler(string message);
+
+    // Create EventArgs class
+    public class TemperatureChangeEventArgs : EventArgs
+    {
+        // property holding the temperature
+        public int Temperature { get; }
+        // Set temperature in constructor
+        public TemperatureChangeEventArgs(int temperature)
+        {
+            Temperature = temperature;
+        }
+    }
 
     public class TemperatureMonitor
     {
-        public event TemperatureChangeHandler? OnTemperatureChange;
+        //public event TemperatureChangeHandler? OnTemperatureChange;
+        // Generic event handler
+        public event EventHandler<TemperatureChangeEventArgs>? TemperatureChange;
 
         private int _temperature;
         public int Temperature
@@ -13,19 +27,21 @@
             get { return _temperature; }
             set
             {
-                _temperature = value;
-                if (_temperature > 30)
+                // _temperature = value;
+                // if (_temperature > 30)
+                if(_temperature != value)
                 {
+                    _temperature = value;
                     // RAISE EVENT
-                    RaiseTemperatureChangedEvent("Temperature is above threshold!");
+                    OnTemperatureChange(new TemperatureChangeEventArgs(value));
 
                 }
             }
         }
 
-        protected virtual void RaiseTemperatureChangedEvent(string message)
+        protected virtual void OnTemperatureChange(TemperatureChangeEventArgs e)
         {
-            OnTemperatureChange?.Invoke(message);
+            TemperatureChange?.Invoke(this, e);
 
         }
 
@@ -34,9 +50,17 @@
        // Subscriber
         public class TemperatureAlert
         {
-            public void OnTemperatureChange(string message)
+            public void OnTemperatureChange(object ?sender, TemperatureChangeEventArgs e)
             {
-                Console.WriteLine("Alert: " + message);
+                Console.WriteLine($"Alert: Temperature is {e.Temperature}, sender is {sender}");
+            }
+        }
+
+        public class TempCoolingAlert
+        {
+            public void OnTemperatureChange(object ?sender, TemperatureChangeEventArgs e)
+            {
+                Console.WriteLine($"TEMP COOLING ALERT: Temperature is {e.Temperature}, sender is {sender}");
             }
         }
 
@@ -48,7 +72,9 @@
             // Create new instances
             TemperatureMonitor monitor = new TemperatureMonitor();
             TemperatureAlert alert = new TemperatureAlert();
-            monitor.OnTemperatureChange += alert.OnTemperatureChange;
+            TempCoolingAlert alert2 = new TempCoolingAlert();
+            monitor.TemperatureChange += alert.OnTemperatureChange;
+            monitor.TemperatureChange += alert2.OnTemperatureChange;
 
             monitor.Temperature = 20;
             // Get user's input
--- EventsApp/obj/Debug/net9.0/EventsApp.AssemblyInfo.cs
+++ EventsApp/obj/Debug/net9.0/EventsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30e4b3f1f8e28eaed3afd93981b370314c1f8f95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3206701b3996181c52d1898e555db7b146a3f2ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
