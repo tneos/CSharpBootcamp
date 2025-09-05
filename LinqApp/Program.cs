e2837ed2d6e946f287806b2f1580343477a355a0
Thomas Neos <tneos@hotmail.com>
2025-09-05 17:40:46 +0100

    Add linq basic example

---
+++ LinqApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 3, 6, 23, 24, 42 };
+
+            OddNumbers(numbers);
+            Console.ReadKey();
+
+        }
+
+        static void OddNumbers(int[] numbers)
+        {
+            // Linq statement
+            IEnumerable<int> oddNumbers = from number in numbers where number % 2 != 0 select number;
+
+            Console.WriteLine(oddNumbers);
+
+            foreach (int i in oddNumbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
